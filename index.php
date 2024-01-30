c8bd833617d3f959d06d6a5c301a9b89b094075e
krapshsa <krapshsa@gmail.com>
2024-01-30 18:57:11 +0800

    Add nginx.

--- index.php
+++ index.php
@@ -5,6 +5,9 @@ use Sabre\DAV;
 
 require_once 'vendor/autoload.php';
 
+ini_set('output_buffering', 'off');
+ini_set('max_execution_time', '300');
+
 $rootDirectory = new DAV\FS\Directory('public');
 $server        = new DAV\Server($rootDirectory);
 
@@ -13,6 +16,12 @@ $server->setBaseUri('/index.php/');
 $server->addPlugin(new FakeLockersPlugin());
 
 $server->addPlugin(new DAV\Browser\Plugin());
-$server->start();
+
+try {
+    $server->start();
+} catch (\Exception $e) {
+    file_put_contents('kevin.log', $e->getMessage(), FILE_APPEND);
+    throw $e;
+}
 
 
